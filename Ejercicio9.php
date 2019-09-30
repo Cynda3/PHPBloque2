a2dda50232bec864c3fd372502ba87856af44ced
Cynda3 <andergonzalez33@gmail.com>
2019-09-30 13:59:24 +0200

    HAciendo ejercicio 9

---
+++ Ejercicio9.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+	<title>Ejericio 9</title>
+</head>
+<body>
+	<?php
+
+	$lados = [];
+
+	for ($i=0; $i < 5; $i++) { 
+		array_push($lados, rand(-10, 10));
+	}
+
+	for ($i=0; $i < count($lados); $i++) { 
+		echo ("AreaCuadrado($lado[$i])");
+	}
+	
+
+	function AreaCuadrado($num){
+		if ($num <= 0) {
+			return $num*$num;
+		}else{
+			return "El lado tiene que ser un numero positivo";
+		}
+	}
+
+	?>
+
+
+</body>
+</html>
