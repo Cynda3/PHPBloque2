946f87e98f484245f900eb3082ce9ed1556fe172
cynda3 <andergonzalez33@gmail.com>
2019-09-30 18:55:16 +0200

    Ejercicio 5 hecho

--- Ejercicio5.php
+++ Ejercicio5.php
@@ -7,6 +7,21 @@
 
 	// Mirar funcion cal-days-in-month(cal_gregorian, 1, 2030)
 	
+	$meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+
+	echo("En el mes de ".$meses[0]." hay ".cal_days_in_month(CAL_GREGORIAN, 1, 2019)." dias este año.<br>");
+	echo("En el mes de ".$meses[1]." hay ".cal_days_in_month(CAL_GREGORIAN, 2, 2019)." dias este año.<br>");
+	echo("En el mes de ".$meses[2]." hay ".cal_days_in_month(CAL_GREGORIAN, 3, 2019)." dias este año.<br>");
+	echo("En el mes de ".$meses[3]." hay ".cal_days_in_month(CAL_GREGORIAN, 4, 2019)." dias este año.<br>");
+	echo("En el mes de ".$meses[4]." hay ".cal_days_in_month(CAL_GREGORIAN, 5, 2019)." dias este año.<br>");
+	echo("En el mes de ".$meses[5]." hay ".cal_days_in_month(CAL_GREGORIAN, 6, 2019)." dias este año.<br>");
+	echo("En el mes de ".$meses[6]." hay ".cal_days_in_month(CAL_GREGORIAN, 7, 2019)." dias este año.<br>");
+	echo("En el mes de ".$meses[7]." hay ".cal_days_in_month(CAL_GREGORIAN, 8, 2019)." dias este año.<br>");
+	echo("En el mes de ".$meses[8]." hay ".cal_days_in_month(CAL_GREGORIAN, 9, 2019)." dias este año.<br>");
+	echo("En el mes de ".$meses[9]." hay ".cal_days_in_month(CAL_GREGORIAN, 10, 2019)." dias este año.<br>");
+	echo("En el mes de ".$meses[10]." hay ".cal_days_in_month(CAL_GREGORIAN, 11, 2019)." dias este año.<br>");
+	echo("En el mes de ".$meses[11]." hay ".cal_days_in_month(CAL_GREGORIAN, 12, 2019)." dias este año.<br>");
 
 	?>
 
